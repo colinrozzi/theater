891faad142613e5c789ab7edeb55095efc7720e0
colin <colinrozzi@gmail.com>
2025-02-21 14:31:33 -0500

    refactor(store): simplify async method calls with sync mutex

--- src/host/host_wrapper.rs
+++ src/host/host_wrapper.rs
@@ -1,30 +1,7 @@
-use crate::chain::ChainEvent;
 use crate::ActorStore;
-use anyhow::Result;
-use serde::Serialize;
-use std::sync::OnceLock;
-use tokio::sync::mpsc;
 use wasmtime::StoreContextMut;
-
-static EVENT_SENDER: OnceLock<mpsc::UnboundedSender<(StoreId, String, Vec<u8>)>> = OnceLock::new();
-
-#[derive(Clone, Hash, Eq, PartialEq)]
-struct StoreId(u128);
-
-pub fn init_host_wrapper() {
-    let (tx, mut rx) = mpsc::unbounded_channel();
-    EVENT_SENDER
-        .set(tx)
-        .expect("Host wrapper already initialized");
-
-    // Spawn a task to process events
-    tokio::spawn(async move {
-        while let Some((store_id, event_type, data)) = rx.recv().await {
-            // Process events...
-            // We'll need to figure out how to get the ActorStore reference here
-        }
-    });
-}
+use serde::Serialize;
+use anyhow::Result;
 
 pub struct HostFunctionBoundary {
     pub function_name: String,
@@ -50,28 +27,22 @@ impl HostFunctionBoundary {
         Return: Serialize + Clone,
         F: FnOnce(Args) -> Result<Return>,
     {
-        // Get the event sender
-        let sender = EVENT_SENDER.get().expect("Host wrapper not initialized");
-
         // Record outbound call
         let args_json = serde_json::to_vec(&args)?;
-        let store_id = StoreId(store.data().id.as_uuid().as_u128()); // We'll need to add this to TheaterId
-        sender.send((
-            store_id.clone(),
+        store.data_mut().record_event(
             format!("{}/{}_call", self.interface_name, self.function_name),
-            args_json,
-        ))?;
+            args_json
+        );
 
         // Execute the host function
         let result = f(args)?;
 
         // Record the return value
         let result_json = serde_json::to_vec(&result)?;
-        sender.send((
-            store_id,
+        store.data_mut().record_event(
             format!("{}/{}_return", self.interface_name, self.function_name),
-            result_json,
-        ))?;
+            result_json
+        );
 
         Ok(result)
     }
@@ -88,4 +59,3 @@ macro_rules! host_func_wrap {
         }
     }};
 }
-
