f8a365d84c840570b3244d68c4e29681bf55ed7d
colin <colinrozzi@gmail.com>
2025-02-22 08:05:29 -0500

    refactor(runtime): remove unused spawn function and clean up WIT interfaces

--- src/host/runtime.rs
+++ src/host/runtime.rs
@@ -1,8 +1,8 @@
 use crate::actor_handle::ActorHandle;
 use crate::config::RuntimeHostConfig;
+use crate::host::host_wrapper::HostFunctionBoundary;
 use crate::messages::TheaterCommand;
 use crate::store::ActorStore;
-use crate::host::host_wrapper::HostFunctionBoundary;
 use anyhow::Result;
 use std::future::Future;
 use std::path::PathBuf;
@@ -35,63 +35,13 @@ impl RuntimeHost {
                 move |mut ctx: wasmtime::StoreContextMut<'_, ActorStore>, (msg,): (String,)| {
                     let id = ctx.data().id.clone();
                     info!("[ACTOR] [{}] [{}] {}", id, name, msg);
-                    
+
                     // Record the log message in the chain
                     let _ = boundary.wrap(&mut ctx, msg.clone(), |_| Ok(()));
                     Ok(())
                 },
             )
             .expect("Failed to wrap log function");
-
-        let boundary = HostFunctionBoundary::new("ntwk:theater/runtime", "spawn");
-        runtime
-            .func_wrap_async(
-                "spawn",
-                move |mut ctx: wasmtime::StoreContextMut<'_, ActorStore>,
-                     (manifest,): (String,)|
-                     -> Box<dyn Future<Output = Result<(String,)>> + Send> {
-                    let store = ctx.data_mut();
-                    let theater_tx = store.theater_tx.clone();
-                    let boundary = boundary.clone();
-                    info!("Spawning actor with manifest: {}", manifest);
-                    
-                    Box::new(async move {
-                        // Record spawn request
-                        let _ = boundary.wrap(&mut ctx, manifest.clone(), |_| Ok(()));
-                        
-                        let (response_tx, response_rx) = tokio::sync::oneshot::channel();
-                        info!("sending spawn command");
-                        match theater_tx
-                            .send(TheaterCommand::SpawnActor {
-                                manifest_path: PathBuf::from(manifest),
-                                response_tx,
-                                parent_id: Some(ctx.data().id.clone()),
-                            })
-                            .await
-                        {
-                            Ok(_) => {
-                                // await the Ok(actor_id) response on the response channel
-                                let actor_id =
-                                    response_rx.await.expect("Failed to get actor id").unwrap();
-                                info!("spawned actor with id: {}", actor_id);
-                                
-                                // Record successful spawn
-                                let actor_id_str = actor_id.to_string();
-                                let _ = boundary.wrap(&mut ctx, actor_id_str.clone(), |_| Ok(()));
-                                Ok((actor_id_str,))
-                            }
-                            Err(e) => {
-                                error!("Failed to send spawn command: {}", e);
-                                // Record spawn failure
-                                let err = format!("Failed to send spawn command: {}", e);
-                                let _ = boundary.wrap(&mut ctx, err.clone(), |_| Ok(()));
-                                Err(anyhow::anyhow!(err))
-                            }
-                        }
-                    })
-                },
-            )
-            .expect("Failed to wrap spawn function");
 
         let boundary = HostFunctionBoundary::new("ntwk:theater/runtime", "get-state");
         runtime
@@ -100,15 +50,17 @@ impl RuntimeHost {
                 move |mut ctx: StoreContextMut<'_, ActorStore>, ()| -> Result<(Vec<u8>,)> {
                     // Record the state request
                     let _ = boundary.wrap(&mut ctx, "state_request", |_| Ok(()));
-                    
+
                     // Return current state
-                    let state = ctx.data().get_last_event()
+                    let state = ctx
+                        .data()
+                        .get_last_event()
                         .map(|e| e.data.clone())
                         .unwrap_or_default();
-                    
+
                     // Record the response
                     let _ = boundary.wrap(&mut ctx, state.clone(), |_| Ok(()));
-                    
+
                     Ok((state,))
                 },
             )
@@ -131,3 +83,4 @@ impl RuntimeHost {
         Ok(())
     }
 }
+
