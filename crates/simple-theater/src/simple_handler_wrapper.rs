4de20d5109d3957ec2b248a3ac128d4497c2fc1b
colin <colinrozzi@gmail.com>
2025-11-16 13:22:31 -0500

    x

--- crates/simple-theater/src/simple_handler_wrapper.rs
+++ crates/simple-theater/src/simple_handler_wrapper.rs
@@ -2,18 +2,66 @@
 // we want to wrap the existing SimpleHandler host functions so that they can be used with
 // HostHandler
 
+use anyhow::Result;
+use std::future::Future;
 use theater::handler::HostHandler;
 use theater::host::SimpleHandler;
+use theater::host::{
+    environment::EnvironmentHost, filesystem::FileSystemHost, framework::HttpFramework,
+    http_client::HttpClientHost, message_server::MessageServerHost, process::ProcessHost,
+    random::RandomHost, runtime::RuntimeHost, store::StoreHost, supervisor::SupervisorHost,
+    timing::TimingHost,
+};
 
-pub struct SimpleHandlerWrapper;
+pub enum SimpleHandlerWrapper {
+    MessageServer(MessageServerHost),
+    Environment(EnvironmentHost),
+    FileSystem(FileSystemHost),
+    HttpClient(HttpClientHost),
+    HttpFramework(HttpFramework),
+    Process(ProcessHost),
+    Runtime(RuntimeHost),
+    Supervisor(SupervisorHost),
+    Store(StoreHost),
+    Timing(TimingHost),
+    Random(RandomHost),
+}
 
 impl HostHandler for SimpleHandlerWrapper {
     fn start(
         &mut self,
         actor_handle: theater::actor::handle::ActorHandle,
         shutdown_receiver: theater::shutdown::ShutdownReceiver,
-    ) -> impl std::future::Future<Output = anyhow::Result<()>> + Send {
-        SimpleHandler::start(self, actor_handle, shutdown_receiver)
+    ) -> impl Future<Output = Result<()>> + Send {
+        match self {
+            SimpleHandlerWrapper::MessageServer(handler) => {
+                handler.start(actor_handle, shutdown_receiver)
+            }
+            SimpleHandlerWrapper::Environment(handler) => {
+                handler.start(actor_handle, shutdown_receiver)
+            }
+            SimpleHandlerWrapper::FileSystem(handler) => {
+                handler.start(actor_handle, shutdown_receiver)
+            }
+            SimpleHandlerWrapper::HttpClient(handler) => {
+                handler.start(actor_handle, shutdown_receiver)
+            }
+            SimpleHandlerWrapper::HttpFramework(handler) => {
+                handler.start(actor_handle, shutdown_receiver)
+            }
+            SimpleHandlerWrapper::Process(handler) => {
+                handler.start(actor_handle, shutdown_receiver)
+            }
+            SimpleHandlerWrapper::Runtime(handler) => {
+                handler.start(actor_handle, shutdown_receiver)
+            }
+            SimpleHandlerWrapper::Supervisor(handler) => {
+                handler.start(actor_handle, shutdown_receiver)
+            }
+            SimpleHandlerWrapper::Store(handler) => handler.start(actor_handle, shutdown_receiver),
+            SimpleHandlerWrapper::Timing(handler) => handler.start(actor_handle, shutdown_receiver),
+            SimpleHandlerWrapper::Random(handler) => handler.start(actor_handle, shutdown_receiver),
+        }
     }
 
     fn name(&self) -> &str {
